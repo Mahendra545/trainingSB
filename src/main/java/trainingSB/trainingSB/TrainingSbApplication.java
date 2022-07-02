34910573b6c53f10848f1c5ec60f36bdeeda2677
Mahendra545 <mahendrasanebunny@gmail.com>
2022-07-02 18:47:56 +0530

    initial commit

---
+++ src/main/java/trainingSB/trainingSB/TrainingSbApplication.java
@@ -0,0 +1,13 @@
+package trainingSB.trainingSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingSbApplication.class, args);
+	}
+
+}
